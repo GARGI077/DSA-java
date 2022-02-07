a9d36189b514c142ec83d58740ccb16862de0cf1
GARGI077 <singhgargi077@gmail.com>
2022-02-07 22:53:14 +0530

    DSA

---
+++ JavaDSA/src/_03_Conditional_loops/_18_Palindrome.java
@@ -0,0 +1,26 @@
+package _03_Conditional_loops;
+
+import java.util.Scanner;
+
+public class _18_Palindrome {
+    public static void main(String[] args) {
+
+        Scanner in= new Scanner(System.in);
+
+        System.out.println("enter the number ");
+        int num=in.nextInt();
+
+        int rev=0,temp=num,rem;
+
+        while(num!=0)
+        {
+            rem=num%10;
+            rev=rev*10+rem;
+            num=num/10;
+        }
+        if(rev==temp)
+        {
+            System.out.println("Number is Palindrome ");
+        }
+    }
+}
